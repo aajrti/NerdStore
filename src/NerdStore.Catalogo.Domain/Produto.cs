04ff38282246171af2aa7d5fee5417b9e7a6505a
aajr.ti <aajr.ti@gmail.com>
2020-07-24 21:04:26 -0300

    Foi criado SuperClass Entity

---
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Produto
+    {
+        public Guid Id { get; set; }
+
+    }     
+    
+    public class Categoria
+    {
+        public Guid Id { get; set; }
+    }
+}
