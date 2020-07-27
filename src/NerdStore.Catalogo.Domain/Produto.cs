389bacfaa59535165703e3c56d9b9313ac01a292
aajr.ti <aajr.ti@gmail.com>
2020-07-27 08:22:52 -0300

    Incluindo objeto de valor Dimensoes

--- src/NerdStore.Catalogo.Domain/Produto.cs
+++ src/NerdStore.Catalogo.Domain/Produto.cs
@@ -7,13 +7,14 @@ namespace NerdStore.Catalogo.Domain
     public class Produto : Entity, IAggregateRoot
     {
         public Produto(
-            string nome, 
-            string descricao, 
-            bool ativo, 
-            decimal valor, 
+            string nome,
+            string descricao,
+            bool ativo,
+            decimal valor,
             Guid categoriaId,
             DateTime dataCadastro,
-            string imagem)
+            string imagem, 
+            Dimensoes dimensoes)
         {
             Nome = nome;
             Descricao = descricao;
@@ -22,9 +23,10 @@ namespace NerdStore.Catalogo.Domain
             CategoriaId = categoriaId;
             DataCadastro = dataCadastro;
             Imagem = imagem;
+            Dimensoes = dimensoes;
 
             Validar();
-        }   
+        }
 
         public Guid CategoriaId { get; private set; }
         public string Nome { get; private set; }
@@ -35,6 +37,7 @@ namespace NerdStore.Catalogo.Domain
         public string Imagem { get; private set; }
         public int QuantidadeEstoque { get; private set; }
         public int QuantidadeMinimaEstoque { get; private set; }
+        public Dimensoes Dimensoes { get; private set; }
         public Categoria Categoria { get; private set; }
 
 
@@ -54,6 +57,7 @@ namespace NerdStore.Catalogo.Domain
         public void DebitarEstoque(int quantidade)
         {
             if (quantidade < 0) quantidade *= -1;
+            if (!PossuiEstoque(quantidade)) throw new DomainException("Estoque insuficiente");
             QuantidadeEstoque -= quantidade;
         }
         
