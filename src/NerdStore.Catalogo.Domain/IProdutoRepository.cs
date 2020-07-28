7b68bc52d1aadc479dc83ab70fe00da67e0a0fe0
aajr.ti <aajr.ti@gmail.com>
2020-07-28 07:19:51 -0300

    Interface de Repositorio de Produto

---
+++ src/NerdStore.Catalogo.Domain/IProdutoRepository.cs
@@ -0,0 +1,23 @@
+﻿using NerdStore.Core.Data;
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public interface IProdutoRepository : IRepository<Produto>
+    {
+        Task<Produto> ObterProduto(Guid id);
+        Task<IEnumerable<Produto>> ObterTodos();
+        Task<IEnumerable<Produto>> ObterProdutosPorCategoria(int codigo);
+        Task<IEnumerable<Categoria>> ObterCategorias();
+
+        void Adicionar(Produto produto);
+        void Atualizar(Produto produto);
+
+        void Adicionar(Categoria categoria);
+        void Atualizar(Categoria categoria);
+    }
+}
