713f718376a20ffd203a6271e19b5f53dce38ca1
aajr.ti <aajr.ti@gmail.com>
2020-07-28 23:21:08 -0300

    Implementacao do evento

--- src/NerdStore.Catalogo.Domain/EstoqueService.cs
+++ src/NerdStore.Catalogo.Domain/EstoqueService.cs
@@ -1,15 +1,19 @@
 ﻿using System;
 using System.Threading.Tasks;
+using NerdStore.Catalogo.Domain.Events;
+using NerdStore.Core.Bus;
 
 namespace NerdStore.Catalogo.Domain
 {
     public class EstoqueService : IEstoqueService
     {
         private readonly IProdutoRepository _produtoRepository;
+        private readonly IMediatrHandler _bus;
 
-        public EstoqueService(IProdutoRepository produtoRepository)
+        public EstoqueService(IProdutoRepository produtoRepository, IMediatrHandler bus)
         {
             _produtoRepository = produtoRepository;
+            _bus = bus;
         }
 
         public async Task<bool> DebitarEstoque(Guid produtoId, int quantidade)
@@ -22,8 +26,12 @@ namespace NerdStore.Catalogo.Domain
 
             produto.DebitarEstoque(quantidade);
 
+            if (produto.QuantidadeEstoque < 10)
+            {
+                await _bus.PublicarEvento(new ProdutoAbaixoEstoqueEvent(produto.Id, produto.QuantidadeEstoque));
+            }
+
             _produtoRepository.Atualizar(produto);
-            
             return await _produtoRepository.UnitOfWork.Commit();
         }
 
