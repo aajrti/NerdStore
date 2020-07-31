c9a8ee8509d618851759a3b42c8230920b9ecb20
aajr.ti <aajr.ti@gmail.com>
2020-07-31 08:06:40 -0300

    Criando agregação de Pedidos

---
+++ src/NerdStore.Vendas.Domain/Pedido.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Vendas.Domain
+{
+    public class Pedido
+    {
+    }
+
+    public class PedidoItem
+    {
+    }
+
+    public interface IPedidoRepository
+    {
+    }
+}
