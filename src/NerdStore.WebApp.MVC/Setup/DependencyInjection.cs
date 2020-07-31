0e911161b89f1bb108f0d7a13837c34b5c60c063
aajr.ti <aajr.ti@gmail.com>
2020-07-31 07:51:22 -0300

    Correção na insjeção de evento estoqueService

--- src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
+++ src/NerdStore.WebApp.MVC/Setup/DependencyInjection.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using MediatR;
+
+using Microsoft.Extensions.DependencyInjection;
 
 using NerdStore.Catalogo.Application.Services;
 using NerdStore.Catalogo.Data;
 using NerdStore.Catalogo.Data.Repository;
 using NerdStore.Catalogo.Domain;
+using NerdStore.Catalogo.Domain.Events;
 using NerdStore.Core.Bus;
 
 namespace NerdStore.WebApp.MVC.Setup
@@ -20,6 +23,8 @@ namespace NerdStore.WebApp.MVC.Setup
             services.AddScoped<IProdutoAppService, ProdutoAppService>();
             services.AddScoped<IEstoqueService, EstoqueService>();
             services.AddScoped<CatalogoContext>();
+
+            services.AddScoped<INotificationHandler<ProdutoAbaixoEstoqueEvent>, ProdutoEventHandler>();
         }
     }
 }
